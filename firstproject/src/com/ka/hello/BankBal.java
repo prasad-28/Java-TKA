d7aaa8c845ec2db53f46177036d3fd207f4ea25b
Prasad <panchalgangaprasad@gmail.com>
2025-09-13 13:22:44 +0530

    new  project added

--- firstproject/src/com/ka/hello/BankBal.java
+++ firstproject/src/com/ka/hello/BankBal.java
@@ -1,36 +1,21 @@
 package com.ka.hello;
 
 public class BankBal {
+
+	//Greatest Among Two no's
+
 	public static void main(String[] args) {
 
-		String CustomerName = "Ram Deshmukh";
-		long customerAccount = 4545454545l;
-		String CustomerDob = "15/08/1990";
-		double accountBalance = 5000;
+		int a = 10;
+		int b = 15; 
 
-		System.out.println("Customer Name: " + CustomerName);
-		System.out.println("--------------------------------");
-
-		System.out.println("Customer Account N0. : " + customerAccount);
-		System.out.println("--------------------------------");
-
-		System.out.println("Customer Date of Birth: " + CustomerDob);
-		System.out.println("--------------------------------");
-		
-	
-
-		// System.out.println(isMinBal);
-
-		if (accountBalance <= 0) {
-			System.out.println("Alert: Insufficient Balance");
-		} else if (accountBalance < 5000) {
-			System.out.println("Please maintain a minimum balance of 5000");
-		} else {
-			System.out.println("Your Bank Balance Is: " + accountBalance);
+		if(a==b) {
+			System.out.println("Equal");
+		}else if(a>b) {
+			System.out.println("B greater");
+		}else {
+			System.out.println("A is greater");
 		}
-		
-		System.out.println("--------------------------------");
-		System.out.println("Thank You Have Great Date");
 
 	}
 
